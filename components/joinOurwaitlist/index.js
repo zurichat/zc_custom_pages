35301ee1a36e0e718258cec1b338506b72a6748a
ayodiya <devayodiya@gmail.com>
2021-10-27 15:21:38 +0100

    wait list page

---
+++ components/joinOurwaitlist/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import HeadingTextWithImage from './components/HeadingTextwithImage';
+import Logo from './components/Logo';
+import RemoteEducation from './components/RemoteEducation';
+import Seamless from './components/Seamless';
+import AccessFiles from './components/AccessFiles';
+import OneWorkspace from './components/OneWorkspace';
+import Footer from './components/Footer';
+
+function JoinOurWaitList() {
+  return (
+    <>
+      <Logo />
+      <HeadingTextWithImage />
+      <Seamless />
+      <RemoteEducation />
+      <AccessFiles />
+      <OneWorkspace />
+      <Footer />
+    </>
+  );
+}
+
+export default JoinOurWaitList;
